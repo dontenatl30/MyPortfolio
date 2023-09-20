81dea849347d428a692be27881c811d20fafd713
LaDonte LeBlanc <donte.11@hotmail.com>
2023-09-19 21:33:59 -0400

    first commit

---
+++ formhandler.php
@@ -0,0 +1,16 @@
+<!-- <php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    
+    $to = "atlanta201626@gmail.com"; // Replace with your email address
+    $headers = "From: $email";
+    
+    mail($to, $subject, $message, $headers);
+}
+> -->
+
+
+
